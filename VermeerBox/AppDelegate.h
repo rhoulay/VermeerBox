7545aa05e642810a41eb6a4d880bfbb3a528f878
He Felix <rhoulay@gmail.com>
2016-03-14 16:32:19 +0800

    Initial Commit

---
+++ VermeerBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VermeerBox
+//
+//  Created by Felix He on 3/14/16.
+//  Copyright © 2016 Felix He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VermeerBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VermeerBox
+//
+//  Created by Felix He on 3/14/16.
+//  Copyright © 2016 Felix He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
